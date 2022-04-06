35317792802ed39d5ae3f2c78e893476ee838091
JTERRAZ9 <jterraz9@ford.com>
2022-04-06 11:16:41 +0200

    Day 5: Loop

---
+++ src/Loop.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Loop {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        for(int i = 1; i < 11; i++){
+            System.out.println(n + " x " + i + " = " + n*i );
+        }
+    }
+
+}
+
