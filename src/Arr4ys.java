b5f4ad90619baed6979f61780760918a901f9af4
JTERRAZ9 <jterraz9@ford.com>
2022-04-08 14:49:48 +0200

    Day 7: Arrays

---
+++ src/Arr4ys.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Arr4ys{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        bufferedReader.close();
+
+
+        for(int i=1; i <= n; i++){
+
+            System.out.print(arr.get(n-i)+" ");
+        }
+
+    }
+}
